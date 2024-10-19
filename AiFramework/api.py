ba95cf2767ecb9272c1514487b9324f265c5e827
sandrotbilisi <atsetsoffc@gmail.com>
2024-10-19 15:14:46 -0700

    feat: Add multi-agent function to AI Framework API, moved some stuff bassicaly

--- AiFramework/api.py
+++ AiFramework/api.py
@@ -20,13 +20,17 @@ client = Swarm()
 # Save User History for ML Memory
 user_histories = {}
 
+# multi agent return
+def muilti_agent():
+    return multi_language_agent
+
 # Create Main Agent
 main_agent = Agent(
     name="Main Event",
     instructions=(
         "You are a helpful agent, writing in TELEGRAM"  
     ),
-    functions=[],
+    functions=[muilti_agent],
 )
 
 # Crate Multi-Language Agent
