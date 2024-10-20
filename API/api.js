13dc031142754f680457206f523522ccad7dd2cb
sandrotbilisi <atsetsoffc@gmail.com>
2024-10-19 19:56:09 -0700

    feat: Implement POST endpoint to create a new fiat account

--- API/api.js
+++ API/api.js
@@ -9,10 +9,35 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+// POST endpoint to create a new fiat account
+// Request body should contain the following parameters:
+// - customerId: The unique ID of the customer
+// - accountNumber: The customer's account number
+// - recipientFullAddress: The full address of the recipient
+// - recipientAddressCountry: The country of the recipient's address
 app.post('/newFiatAccount', (req, res) => {
-    res.send('New Fiat Account');
-    // TODO : Implement newFiatAccount
+    const { customerId, accountNumber, recipientFullAddress, recipientAddressCountry } = req.body;
+
+    const response = axios.post('https://actions-registry.dialectapi.to/all', 
+        {
+            customerId,
+            type : "SEPA",
+            fiatAccountFields : {
+                accountNumber,
+                recipientFullAddress,
+                recipientAddressCountry
+            }
+        }
+    )
+
+    if (response.status !== 200) {
+        res.status(400).send('Error creating new fiat account');
+    }
+    
+    res.send(response.data);
 });
+
+
 
 
 app.listen(port, () => {
