2438d203c6364fd4342e074a270e2bd43a0b2c78
sandrotbilisi <atsetsoffc@gmail.com>
2024-10-19 18:15:32 -0700

    chore: Update API package.json with express dependency, added simple TODO

---
+++ API/api.js
@@ -0,0 +1,19 @@
+// import
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.post('/newFiatAccount', (req, res) => {
+    res.send('New Fiat Account');
+    // TODO : Implement newFiatAccount
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
